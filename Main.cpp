6c572c0f27094188e4672af5094dde1db02003bb
Артём <sh0n556@bk.ru>
2025-08-06 08:32:08 +0300

    some initialization

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,15 @@
+#include <Type_Manager.h>
+#include <Object_Constructor.h>
+
+#include <LScript_Registration.h>
 
 
 int main()
 {
+    LV::Type_Manager::register_basic_types();
+    LV::Object_Constructor object_constructor;
 
+    LScript::register_types(object_constructor);
 
     return 0;
 }
