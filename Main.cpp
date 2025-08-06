0cb808e80d34fd05a3a7ccc05a9df3003ee8788c
Артём <sh0n556@bk.ru>
2025-08-06 11:45:53 +0300

    testing simple script code parsing

--- Main.cpp
+++ Main.cpp
@@ -3,6 +3,8 @@
 
 #include <LScript_Registration.h>
 
+#include <Script.h>
+
 
 int main()
 {
@@ -10,6 +12,19 @@ int main()
     LV::Object_Constructor object_constructor;
 
     LScript::register_types(object_constructor);
+
+
+    LV::MDL_Reader reader;
+    reader.parse_file("../Resources/Script_Test");
+
+    LScript::Script_Stub* stub = (LScript::Script_Stub*)object_constructor.construct(reader.get_stub("Script_Test"));
+    LScript::Script* script = LScript::Script_Stub::construct_from(stub);
+
+
+
+    delete stub;
+    delete script;
+
 
     return 0;
 }
